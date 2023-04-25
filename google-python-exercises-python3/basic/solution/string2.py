c0de8072c48d5d9d24a9560f2a84894dd596f364
BjornFJohansson <bjornjobb@gmail.com>
2023-04-25 07:12:22 +0100

    updated google-python-exercises-python3

--- google-python-exercises-python3/basic/solution/string2.py
+++ google-python-exercises-python3/basic/solution/string2.py
@@ -17,7 +17,14 @@
 # Return the resulting string.
 def verbing(s):
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  if len(s) >= 3:
+    if s[-3:] != 'ing': s = s + 'ing'
+    else: s = s + 'ly'
+  return s
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
 
 
 # E. not_bad
@@ -30,7 +37,15 @@ def verbing(s):
 # This dinner is good!
 def not_bad(s):
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  n = s.find('not')
+  b = s.find('bad')
+  if n != -1 and b != -1 and b > n:
+    s = s[:n] + 'good' + s[b+3:]
+  return s
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
 
 
 # F. front_back
@@ -42,7 +57,18 @@ def not_bad(s):
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  # Figure out the middle position of each string.
+  a_middle = len(a) / 2
+  b_middle = len(b) / 2
+  if len(a) % 2 == 1:  # add 1 if length is odd
+    a_middle = a_middle + 1
+  if len(b) % 2 == 1:
+    b_middle = b_middle + 1 
+  return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
 
 
 # Simple provided test() function used in main() to print
--- google-python-exercises-python3/basic/solution/wordcount.py
+++ google-python-exercises-python3/basic/solution/wordcount.py
@@ -45,7 +45,53 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
-###
+#### LAB(begin solution)
+
+def word_count_dict(filename):
+  """Returns a word/count dict for this filename."""
+  # Utility used by count() and Topcount().
+  word_count = {}  # Map each word to its count
+  input_file = open(filename, 'r')
+  for line in input_file:
+    words = line.split()
+    for word in words:
+      word = word.lower()
+      # Special case if we're seeing this word for the first time.
+      if not word in word_count:
+        word_count[word] = 1
+      else:
+        word_count[word] = word_count[word] + 1
+  input_file.close()  # Not strictly required, but good form.
+  return word_count
+
+
+def print_words(filename):
+  """Prints one per line '<word> <count>' sorted by word for the given file."""
+  word_count = word_count_dict(filename)
+  words = sorted(word_count.keys())
+  for word in words:
+    print(word, word_count[word])
+
+
+def get_count(word_count_tuple):
+  """Returns the count from a dict word/count tuple  -- used for custom sort."""
+  return word_count_tuple[1]
+
+
+def print_top(filename):
+  """Prints the top count listing for the given file."""
+  word_count = word_count_dict(filename)
+
+  # Each item is a (word, count) tuple.
+  # Sort them so the big counts are first using key=get_count() to extract count.
+  items = sorted(list(word_count.items()), key=get_count, reverse=True)
+
+  # Print the first 20
+  for item in items[:20]:
+    print(item[0], item[1])
+
+##### LAB(end solution)
+
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
